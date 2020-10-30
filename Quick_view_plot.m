72ecda890092b43c8e67b72879cce4854c4a162c
kesireddy.adarsh <kesireddy.adarsh.k@gmail.com>
2020-10-30 14:30:29 -0500

    "Graphs"

--- Quick_view_plot.m
+++ Quick_view_plot.m
@@ -7,10 +7,10 @@ read_1 = importdata('/home/ak/Documents/gccProjects/Unicycle/Unicycle_Location_7
 %read_3 = importdata('/home/ak/Documents/gccProjects/Unicycle/Location_120_0_2.txt');
 
 reward_1 = importdata('/home/ak/Documents/gccProjects/Unicycle/rewards_0.txt');
-reward_100 = importdata('/home/ak/Documents/gccProjects/Unicycle/rewards_100.txt');
-reward_150 = importdata('/home/ak/Documents/gccProjects/Unicycle/rewards_180.txt');
+reward_100 = importdata('/home/ak/Documents/gccProjects/Unicycle/rewards_1000.txt');
+reward_150 = importdata('/home/ak/Documents/gccProjects/Unicycle/rewards_2999.txt');
 
-mean_1 = [mean(reward_1(:,5)),mean(reward_1(:,6)),mean(reward_1(:,7))]
+mean_1 = mean(reward_1(:,6))
 mean_100 = mean(reward_100(:,6))
 mean_150 = mean(reward_150(:,6))
 %scatter(read_1(:,2),read_1(:,3))
