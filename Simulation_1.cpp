1dc4ace955f8ecd66493a05a4dfccf67ad9f870b
kesireddy.adarsh <kesireddy.adarsh.k@gmail.com>
2020-11-20 10:54:42 -0600

    "Includes plot .ods file"

--- Simulation_1.cpp
+++ Simulation_1.cpp
@@ -695,7 +695,7 @@ void unicycle_movement(vector<population>* teams, int population_number, int  te
  *******************************************************/
 
 void simulation_team(vector<population>* teams, vector<Environment>* p_environment,int generation,int number_of_obstacles, vector<vector<double>>* p_stat, double distance_between_rover,int number_of_routes,int number_of_rovers){
-    int max_time_step = 500;
+    int max_time_step = 1000;
     //cout<<p_environment->size()<<endl;
     for (int population_number =0 ; population_number< teams->size(); population_number++) {
         for (int team_value = 0 ; team_value < number_of_routes ; team_value++) {
@@ -973,8 +973,7 @@ void distance_team(vector<population>* teams, double distance_between_rover, dou
                 teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).difference_target_1 = teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).global_target_1 - teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).shortest_target_distance;
                 
                 //teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).difference_target_2 = teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).global_target_2 - teams->at(population_number).teamRover.at(rover).new_network.at(teams->at(population_number).path_numbers.at(team_value).at(rover)).shortest_target_distance_1;
-                
-              number_of_generations  
+                  
                 int fitness_case_number = 0;
                 switch (fitness_case_number) {
                     case 0:
@@ -2608,7 +2607,7 @@ void run_simulation_function(){
     Environment temp_en;
     p_en_vector->push_back(temp_en);
     
-    int number_of_generations = 500;
+    int number_of_generations = 5000;
 
     create_environment(number_of_obstacles, number_of_poi, p_en_vector );
     // Prints the environment
